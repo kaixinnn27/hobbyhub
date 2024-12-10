8a54a5c9b8442b4e4d93db047fac881fe9f56d04
junsheng <junshengchoo57@gmail.com>
2024-12-10 22:30:19 +0800

    fix: remove used file

--- app/src/main/java/com/example/hobbyhub/authentication/view/LoginFragment.kt
+++ app/src/main/java/com/example/hobbyhub/authentication/view/LoginFragment.kt
@@ -44,7 +44,7 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         auth = Firebase.auth
--- app/src/main/java/com/example/hobbyhub/utility/BiometricAuthListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hobbyhub.utility
-
-import androidx.biometric.BiometricPrompt
-
-interface BiometricAuthListener {
-    fun onBiometricAuthenticateSuccess(result: BiometricPrompt.AuthenticationResult)
-    fun onBiometricAuthenticateError(errorCode: Int, errString: String)
-    fun onBiometricAuthenticateFailed()
-}
